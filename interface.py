2069583d43b8fbd0a2a9af3e9f2012c15e8b116a
Bruno de Moraes <brunoestudolog@gmail.com>
2023-07-10 21:04:59 -0300

    Função de carregamentos provenientes de Mixins.

--- interface.py
+++ interface.py
@@ -10,13 +10,22 @@ from dados import Dados
 
 class interface(Contas, Dados):
     def __init__(self, tela):
-
         self.frame_mestre = LabelFrame(tela, padx=0, pady=0)
         self.frame_mestre.pack(padx=200, pady=100)
 
-        self.teste = Button(self.frame_mestre, text='Executar', command=self.relatorio_inicial)
+        self.teste = Button(self.frame_mestre, text='Executar', command=self.carregar_dados)
         self.teste.pack()
 
+        self.teste = Button(self.frame_mestre, text='Listar', command=self.praaaa)
+        self.teste.pack()
+
+    def carregar_dados(self):
+        self.listar_pagamentos()
+        self.listar_contas()
+
+    def praaaa(self):
+        for chave, valor in sorted(self.itens_somados.items()):
+            print(f'{chave} - {valor}')
 
 
 if __name__ == '__main__':
