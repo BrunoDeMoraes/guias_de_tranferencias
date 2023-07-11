e726782c63a579957ef660daff472b606eae0af4
Bruno de Moraes <brunoestudolog@gmail.com>
2023-07-10 21:04:00 -0300

    Função de rotina para listagem de pagamentos, sepração da função de valor por extenso e inclusão de campo Conta.

--- dados.py
+++ dados.py
@@ -7,47 +7,51 @@ class Dados:
         print(df.columns)
         filtro = df.loc[df['Nº DANFE'].notna() & df['Nº TED'].isna()]
         self.pagamentos = filtro.filter(
-            ['Cotação', 'Empresa ', 'Nº DANFE', 'V. Total', 'Nº TED', 'Nº de processo SEI']
+            ['Cotação', 'Empresa ', 'Nº DANFE', 'V. Total', 'Nº TED', 'Nº de processo SEI', 'Conta']
         )
-        print(self.pagamentos)
+        #print(self.pagamentos)
 
     def valor_de_pagamento(self):
-        duplicados = self.pagamentos[self.pagamentos.duplicated(subset=self.pagamentos.columns.drop('V. Total'), keep=False)]
-        print(duplicados)
+        duplicados = self.pagamentos[self.pagamentos.duplicated(subset=self.pagamentos.columns.drop('V. Total', 'Conta'), keep=False)]
+        #print(duplicados)
+        self.itens_somados = {}
         checagem = []
-        itens_somados = {}
         for indice, linha in duplicados.iterrows():
             palavra_checagem = str(linha['Cotação']) + '-' + str(linha['Empresa ']) + '-' + str(linha['Nº DANFE'])
             if palavra_checagem in checagem:
                 continue
             else:
                 checagem.append(palavra_checagem)
-                print(palavra_checagem)
+                #print(palavra_checagem)
                 duplicados_subset1 = duplicados[
                     (duplicados['Cotação'] == linha['Cotação']) &
                     (duplicados['Empresa '] == linha['Empresa ']) &
                     (duplicados['Nº DANFE'] == linha['Nº DANFE'])
                 ]
-                print(duplicados_subset1)
+                #print(duplicados_subset1)
                 soma = duplicados_subset1['V. Total'].sum()
-                print(f'Soma dos itens da NF R$ {soma}')
+                #print(f'Soma dos itens da NF R$ {soma}')
                 descricao = palavra_checagem.split('-')
-                itens_somados[palavra_checagem] = [descricao[0], descricao[1], descricao[2], soma, linha['Nº de processo SEI']]
+                self.itens_somados[palavra_checagem] = [descricao[0], descricao[1], descricao[2], soma, linha['Nº de processo SEI'], linha['Conta']]
         for indice, linha in self.pagamentos.iterrows():
             palavra_checagem = str(linha['Cotação']) + '-' + str(linha['Empresa ']) + '-' + str(linha['Nº DANFE'])
             if palavra_checagem in checagem:
                 continue
             else:
                 checagem.append(palavra_checagem)
-                print(palavra_checagem)
+                #print(palavra_checagem)
                 descricao = palavra_checagem.split('-')
-                itens_somados[palavra_checagem] = [descricao[0], descricao[1], descricao[2], linha['V. Total'], linha['Nº de processo SEI']]
-        print(checagem)
-        for chave, valor in sorted(itens_somados.items()):
+                self.itens_somados[palavra_checagem] = [descricao[0], descricao[1], descricao[2], linha['V. Total'], linha['Nº de processo SEI'], linha['Conta']]
+        #print(checagem)
+        self.valor_por_extenso()
+        return self.itens_somados
+
+
+    def valor_por_extenso(self):
+        for chave, valor in sorted(self.itens_somados.items()):
             extenso = num2words(valor[3], lang='pt_BR', to='currency')
             valor.append(extenso)
             print(f'{chave} - {valor}')
-        return itens_somados
 
     def fornecedores(self):
         df = pd.read_excel("Matrix_2023_HRG.xlsx", sheet_name='Fornecedores')
@@ -55,6 +59,8 @@ class Dados:
         for indice, linha in df.iterrows():
             a = linha.to_list()
             self.empresas[a[0]] = a[1:]
+            print(self.empresas)
+
     def separador(self, dados):
         for indice, linha in dados.iterrows():
             a = linha.to_list()
@@ -64,6 +70,6 @@ class Dados:
             else:
                 pass
 
-    def relatorio_inicial(self):
+    def listar_pagamentos(self):
         self.dados_de_pagamento()
         self.valor_de_pagamento()
