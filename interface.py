8891310f856d7336e8ab5e77779af35f3123e59c
Bruno de Moraes <brunoestudolog@gmail.com>
2023-07-11 13:55:17 -0300

    Inserção de Radiobuttons.

--- interface.py
+++ interface.py
@@ -9,25 +9,26 @@ from contas import Contas
 from dados import Dados
 
 class interface(Contas, Dados):
+
+    ORIGEM = [("SRSSU", "SRSSU"), ("APS", "APS")]
     def __init__(self, tela):
         self.frame_mestre = LabelFrame(tela, padx=0, pady=0)
         self.frame_mestre.pack(padx=200, pady=100)
 
-        self.teste = Button(self.frame_mestre, text='Executar', command=self.carregar_dados)
-        self.teste.pack()
+        local = StringVar()
+        local.set("SRSSU")
 
-        self.teste = Button(self.frame_mestre, text='Listar', command=self.praaaa)
+        for legenda, valor in interface.ORIGEM:
+            self.conta_origem = Radiobutton(tela, text=legenda, variable=local, value=valor)
+            self.conta_origem.pack()
+
+        self.teste = Button(self.frame_mestre, text='Executar', command=self.carregar_dados)
         self.teste.pack()
 
     def carregar_dados(self):
         self.listar_pagamentos()
         self.listar_contas()
-
-    def praaaa(self):
-        for chave, valor in sorted(self.itens_somados.items()):
-            print(f'{chave} - {valor}')
-
-
+    
 if __name__ == '__main__':
     tela = Tk()
     objeto_tela = interface(tela)
