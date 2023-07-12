c55d713dfc0690bf143e9e60d41148800311d27f
Bruno de Moraes <brunoestudolog@gmail.com>
2023-07-12 11:57:50 -0300

    reorganização de funções trazidas da interface

--- dados.py
+++ dados.py
@@ -4,12 +4,10 @@ from num2words import num2words
 class Dados:
     def dados_de_pagamento(self):
         df = pd.read_excel("Matrix_2023_HRG.xlsx", skiprows=[0])
-        print(df.columns)
         filtro = df.loc[df['Nº DANFE'].notna() & df['Nº TED'].isna()]
         self.pagamentos = filtro.filter(
             ['Cotação', 'Empresa ', 'Nº DANFE', 'V. Total', 'Nº TED', 'Nº de processo SEI', 'Conta']
         )
-        #print(self.pagamentos)
 
     def valor_de_pagamento(self):
         duplicados = self.pagamentos[self.pagamentos.duplicated(subset=['Cotação', 'Empresa ', 'Nº DANFE'], keep=False)]
@@ -21,15 +19,12 @@ class Dados:
                 continue
             else:
                 checagem.append(palavra_checagem)
-                #print(palavra_checagem)
                 duplicados_subset1 = duplicados[
                     (duplicados['Cotação'] == linha['Cotação']) &
                     (duplicados['Empresa '] == linha['Empresa ']) &
                     (duplicados['Nº DANFE'] == linha['Nº DANFE'])
                 ]
-                #print(duplicados_subset1)
                 soma = duplicados_subset1['V. Total'].sum()
-                #print(f'Soma dos itens da NF R$ {soma}')
                 descricao = palavra_checagem.split('-')
                 self.itens_somados[palavra_checagem] = [descricao[0], descricao[1], descricao[2], soma, linha['Nº de processo SEI'], linha['Conta']]
         for indice, linha in self.pagamentos.iterrows():
@@ -38,19 +33,15 @@ class Dados:
                 continue
             else:
                 checagem.append(palavra_checagem)
-                #print(palavra_checagem)
                 descricao = palavra_checagem.split('-')
                 self.itens_somados[palavra_checagem] = [descricao[0], descricao[1], descricao[2], linha['V. Total'], linha['Nº de processo SEI'], linha['Conta']]
-        #print(checagem)
         self.valor_por_extenso()
-        return self.itens_somados
 
 
     def valor_por_extenso(self):
         for chave, valor in sorted(self.itens_somados.items()):
             extenso = num2words(valor[3], lang='pt_BR', to='currency')
             valor.append(extenso)
-            print(f'{chave} - {valor}')
 
     def fornecedores(self):
         df = pd.read_excel("Matrix_2023_HRG.xlsx", sheet_name='Fornecedores')
@@ -69,6 +60,18 @@ class Dados:
             else:
                 pass
 
+    def valores_formatados(self):
+        valores_impressao = ""
+        for i in self.itens_somados.values():
+            valores_impressao = valores_impressao + f'\n{i}\n'
+        print(valores_impressao)
+        return valores_impressao
+
     def listar_pagamentos(self):
         self.dados_de_pagamento()
         self.valor_de_pagamento()
+        self.valores_formatados()
+
+    def checa_carregamento(self):
+        return hasattr(self, 'itens_somados')
+ 
