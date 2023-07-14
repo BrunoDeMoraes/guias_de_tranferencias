316bd72e0ff5037b14adedeb8f3dc1c6f2acedfa
Bruno de Moraes <brunoestudolog@gmail.com>
2023-07-13 21:08:03 -0300

    Recebimento de Mixins e esboço de função de separação de guias.

--- relatorio.py
+++ relatorio.py
@@ -3,10 +3,23 @@ from reportlab.lib.pagesizes import A4
 
 from contas import Contas
 from dados import Dados
-class Relatorio:
-    def __init__(self, ):
-        pass
+class Relatorio(Contas, Dados):
+    def __init__(self):
+        self.lis = []
 
+    def separador(self):
+        fornecedores = self.fornecedores()
+        self.dados_de_pagamento()
+        pagamentos = self.valor_de_pagamento().values()
+        for pagamento in pagamentos:
+            empresa = pagamento[1]
+            banco = fornecedores[pagamento[1]][5]
+            if (banco) == "BRB":
+                print(f"{empresa} - É BRB, porra! Transferência intena")
+            else:
+                print(f"{empresa} - Banco: {banco} - melhor fazer uma TED!, ")
 
-    def gerar_ted(self):
-        pass
+if __name__ == '__main__':
+    r = Relatorio()
+    r.separador()
+    #r.fornecedores()
