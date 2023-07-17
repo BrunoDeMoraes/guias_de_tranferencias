b668727e4b23fd6aa40195ae9bad678d4daa4652
Bruno de Moraes <brunoestudolog@gmail.com>
2023-07-17 19:32:23 -0300

    identificadores de campos da guia de TED

--- relatorio.py
+++ relatorio.py
@@ -52,6 +52,16 @@ class Relatorio(Contas, Dados):
             )
 
             cnv.rect(self.mm(126), self.mm(279 - contador), width=self.mm(70), height=self.mm(7))
+            cnv.rect(self.mm(40), self.mm(204 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(70), self.mm(204 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(40), self.mm(199 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(70), self.mm(199 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(140), self.mm(204 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(170), self.mm(204 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(140), self.mm(199 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(170), self.mm(199 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(25), self.mm(194 - contador), width=self.mm(3), height=self.mm(3))
+            cnv.rect(self.mm(105), self.mm(194 - contador), width=self.mm(3), height=self.mm(3))
 
             cnv.setFont("Times-Roman", 7)
             cnv.drawString(self.mm(10), self.mm(265 - contador), "Banco")
@@ -62,11 +72,40 @@ class Relatorio(Contas, Dados):
             cnv.drawString(self.mm(121), self.mm(265 - contador), "Agência")
             cnv.drawString(self.mm(148), self.mm(265 - contador), "Nº Conta Favorecido")
             cnv.drawString(self.mm(174), self.mm(265 - contador), "Valor")
+            cnv.drawString(self.mm(10), self.mm(255 - contador), "Nome do(s) Remetente(s)")
+            cnv.drawString(self.mm(104), self.mm(255 - contador), "Nome do(s) Destinatário(s)")
+            cnv.drawString(self.mm(10), self.mm(245 - contador), "CNPJ/CPF(s)")
+            cnv.drawString(self.mm(104), self.mm(245 - contador), "CNPJ/CPF(s)")
+            cnv.drawString(self.mm(10), self.mm(235 - contador), "Nº identidade / Órgão expedidor / UF")
+            cnv.drawString(self.mm(104), self.mm(235 - contador), "Valor por Extenso")
+            cnv.drawString(self.mm(10), self.mm(225 - contador), "Endereço")
+            cnv.drawString(self.mm(10), self.mm(215 - contador), "Telefone(s)")
+            cnv.drawString(self.mm(104), self.mm(215 - contador), "Nº identidade / Órgão expedidor / UF")
+            cnv.drawString(self.mm(10), self.mm(205 - contador), "Tipo Pessoa Debitada")
+            cnv.drawString(self.mm(10), self.mm(200 - contador), "Tipo Conta Debitada")
+            cnv.drawString(self.mm(104), self.mm(205 - contador), "Tipo Pessoa Creditada")
+            cnv.drawString(self.mm(104), self.mm(200 - contador), "Tipo Conta Creditada")
+            cnv.drawString(self.mm(10), self.mm(195 - contador), "Finalidade")
+            cnv.drawString(self.mm(10), self.mm(190 - contador), "Histórico")
 
-
+            cnv.setFont("Times-Roman", 8)
+            cnv.drawString(self.mm(45), self.mm(204 - contador), "Pesssoa Física")
+            cnv.drawString(self.mm(75), self.mm(204 - contador), "Pesssoa Jurídica")
+            cnv.drawString(self.mm(45), self.mm(199 - contador), "Conta Corrente")
+            cnv.drawString(self.mm(75), self.mm(199 - contador), "Conta Poupança")
+            cnv.drawString(self.mm(145), self.mm(204 - contador), "Pesssoa Física")
+            cnv.drawString(self.mm(175), self.mm(204 - contador), "Pesssoa Jurídica")
+            cnv.drawString(self.mm(145), self.mm(199 - contador), "Conta Corrente")
+            cnv.drawString(self.mm(175), self.mm(199 - contador), "Conta Poupança")
+            cnv.drawString(self.mm(30), self.mm(194 - contador), "00001 - Pagamento de ipostos, tributos e taxas")
+            cnv.drawString(self.mm(110), self.mm(194 - contador), "00005 - Pagamentos de fornecedores")
+            cnv.drawString(self.mm(40), self.mm(189 - contador), "SEI:")
+            cnv.drawString(self.mm(110), self.mm(189 - contador), "Cotação:")
+            cnv.drawString(self.mm(150), self.mm(189 - contador), "DANFE:")
 
             cnv.setFont("Times-Bold", 8)
             cnv.drawString(self.mm(9), self.mm(275 - contador), "ISPB -00.000.208")
+
 
             cnv.setFont("Times-Bold", 10)
             cnv.drawString(self.mm(115), self.mm(275 - contador), 'Transferência Eletrônica Disponível - TED -"E"')
@@ -82,9 +121,9 @@ class Relatorio(Contas, Dados):
             cnv.line(self.mm(25), self.mm(268 - contador), self.mm(25), self.mm(258 - contador)) #divisão linha 1.1
             cnv.line(self.mm(52), self.mm(268 - contador), self.mm(52), self.mm(258 - contador)) #divisão linha 1.2
             cnv.line(self.mm(78), self.mm(268 - contador), self.mm(78), self.mm(258 - contador)) #divisão linha 1.3
-            cnv.line(self.mm(119), self.mm(268 - contador), self.mm(119), self.mm(258 - contador)) #divisão linha 1.3
-            cnv.line(self.mm(146), self.mm(268 - contador), self.mm(146), self.mm(258 - contador)) #divisão linha 1.3
-            cnv.line(self.mm(172), self.mm(268 - contador), self.mm(172), self.mm(258 - contador)) #divisão linha 1.3
+            cnv.line(self.mm(119), self.mm(268 - contador), self.mm(119), self.mm(258 - contador)) #divisão linha 1.4
+            cnv.line(self.mm(146), self.mm(268 - contador), self.mm(146), self.mm(258 - contador)) #divisão linha 1.5
+            cnv.line(self.mm(172), self.mm(268 - contador), self.mm(172), self.mm(258 - contador)) #divisão linha 1.6
             cnv.line(self.mm(8), self.mm(258 - contador), self.mm(196), self.mm(258 - contador)) #linha horizontal 1
             cnv.line(self.mm(8), self.mm(248 - contador), self.mm(196), self.mm(248 - contador)) #linha horizontal 2
             cnv.line(self.mm(8), self.mm(238 - contador), self.mm(196), self.mm(238 - contador)) #linha horizontal 3
