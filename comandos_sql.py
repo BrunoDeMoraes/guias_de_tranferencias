78412b25a1f9f1837cc8b6d48e57d43a0be9fd91
Bruno de Moraes <brunoestudolog@gmail.com>
2023-08-17 16:46:14 -0300

    Conversão de comandos de criação de tabelas em lista.

--- comandos_sql.py
+++ comandos_sql.py
@@ -1,3 +1,10 @@
-BD_CONTA = 'CREATE TABLE contas (origem text, recurso text, tipo text, banco text, agencia text, numero text, cnpj texto)'
+TABELAS = (
+    'CREATE TABLE contas (origem text, recurso text, tipo text, banco text, agencia text, numero text, cnpj texto)',
+    'CREATE TABLE urls (variavel text, url text)'
+)
 
-BD_URLS = 'CREATE TABLE urls (variavel text, url text)'
+CONTAS = ('SELECT * FROM contas')
+
+URLS = ('SELECT * FROM urls')
+
+CONSULTA_TABELAS = "SELECT name FROM sqlite_master WHERE type='table';"
