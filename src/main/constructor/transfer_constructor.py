8896490ce5afb5b278d77f76a0c437f2e6ef2438
Bruno de Moraes <brunoestudolog@gmail.com>
2024-05-15 19:48:12 -0300

    integração do constructor com a interface

--- src/main/constructor/transfer_constructor.py
+++ src/main/constructor/transfer_constructor.py
@@ -1,11 +1,16 @@
 from typing import Dict
 # import dados_da_view
 # import transfer_controller
+from src.controllers.dados_de_pagamento_controller import DadosDePagamentoController
 
 def transfer_constructor(entrada: Dict):
-    pass
 
-    # dados_de_entrada(origem, data_de_pagamento)
+    fonte = '//srv-fs/HRG_GEOF/GEOF/PAGAMENTOS/Fontes/Matrix_2023_HRG.xlsx'
+    teste = DadosDePagamentoController()
+    pagamentos = teste.aglutinar_por_empresa(fonte)
+
+
+    #dados_de_entrada(origem, data_de_pagamento)
     #
     #
     #
