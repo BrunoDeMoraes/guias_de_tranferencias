39f99af5d48041876942051013b4f7ad4ea7b6f4
Bruno de Moraes <brunoestudolog@gmail.com>
2024-05-24 15:57:54 -0300

    Adequação para chamada de forma antiga da classe geradora de view

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 from tkinter import *
 
-from src.views.Gerador_de_view import Gerador_de_views
+from src.views.Gerador_de_view import Interface
 
 if __name__ == '__main__':
-    Gerador_de_views()
+    # Gerador_de_views()
 
-    # tela = Tk()
-    # tela.geometry("300x500")
-    # tela.resizable(0, 0)
-    # objeto_tela = Gerador_de_views(tela)
-    # tela.title()
-    # tela.config(menu=objeto_tela.menu)
-    # tela.mainloop()
+    tela = Tk()
+    tela.geometry("300x500")
+    tela.resizable(0, 0)
+    objeto_tela = Interface(tela)
+    tela.title()
+    tela.config(menu=objeto_tela.menu)
+    tela.mainloop()
 
