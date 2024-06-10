9308f9220958cbf106a5f41ff3dcf4b753391940
Bruno de Moraes <brunoestudolog@gmail.com>
2024-06-10 15:45:43 -0300

    Conversão de valores float em string formatada

--- src/controllers/transfer_contoller.py
+++ src/controllers/transfer_contoller.py
@@ -32,17 +32,19 @@ class TransferController(InterfaceController):
                 dados_empresa = self.fornecedores.retorna_empresa(pagamento[0], fonte)
                 conta_origem = self.contas.pegar_conta(self.origem, pagamento[0][-2:])
                 valor_total = self.somar_indice(pagamento[1], 3)
+                self.converter_valores_em_string(pagamento[1])
                 transferencia['Empresa'] = pagamento[0]
                 transferencia['Pagamentos'] = pagamento[1]
                 transferencia['Dados_empresa'] = dados_empresa
                 transferencia['Conta_origem'] = conta_origem[0]
-                transferencia['Valor_total'] = valor_total
+                transferencia['Valor_total'] = self.formartar_valor(valor_total)
                 transferencias.append(transferencia)
         for p in transferencias:
             for pag in p.items():
                 print(f'{pag[0]}: {pag[1]}')
             print('\n')
         return transferencias
+
 
     def somar_indice(self, pagamentos: List, indice) -> float:
         soma = 0
@@ -51,13 +53,17 @@ class TransferController(InterfaceController):
         total = self.formartar_valor(soma)
         return soma
 
-    # def somar_indices(self, pagamentos, *args):
-    #     soma_indices = []
-    #     for indice in args:
-    #         soma = self.somar_indice(pagamentos, indice)
-    #         soma_indices.append(soma)
-    #     total = sum(soma_indices)
-    #     return total
+
+    def converter_valores_em_string(self, pagamentos: List):
+        for pagamento in pagamentos:
+            indice = 0
+            for dado in pagamento:
+                print(f'Dado {dado}')
+                if isinstance(dado, float):
+                    valor_formatado = self.formartar_valor(dado)
+                    pagamento[indice] = valor_formatado
+                indice += 1
+
 
     def soma_iss_ir(self, pagamentos: list):
         for pagamento in pagamentos:
