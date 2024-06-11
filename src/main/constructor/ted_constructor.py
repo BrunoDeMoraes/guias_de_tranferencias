0f9dd637fb00dd600994a0714f09bfa1781a51bd
Bruno de Moraes <brunoestudolog@gmail.com>
2024-06-11 19:20:39 -0300

    Adequações para gerar objetos especificos do construtor

--- src/main/constructor/ted_constructor.py
+++ src/main/constructor/ted_constructor.py
@@ -3,7 +3,6 @@ from typing import Dict
 from src.models.repository.dados_de_conta import DadosDeContas
 from src.models.repository.dados_de_fornecedores import DadosDeFornecedores
 from src.models.repository.dados_de_pagamento_repository import DadosDePagamentoRepository
-
 from src.views.guia_de_ted import GuiaDeTED
 from src.controllers.ted_controller import TedController
 
