d3b1dafc5cbd552db98cdf3b71a23e325155fa75
Bruno de Moraes <brunoestudolog@gmail.com>
2024-07-11 17:53:07 -0300

    Alterações iniciais em constructor e controller de transferencias interna.

--- src/main/constructor/interna_constructor.py
+++ src/main/constructor/interna_constructor.py
@@ -1,20 +1,20 @@
 from typing import Dict
 
 from src.models.repository.dados_de_conta import DadosDeContas
-from src.models.repository.dados_de_fornecedores import DadosDeFornecedores
-from src.models.repository.dados_de_pagamento_repository import DadosDePagamentoRepository
+# from src.models.repository.dados_de_fornecedores import DadosDeFornecedores
+# from src.models.repository.dados_de_pagamento_repository import DadosDePagamentoRepository
 from src.views.guia_de_transferencia import GuiaDeTransferencia
-from src.controllers.transfer_contoller import TransferController
+from src.controllers.tr_interna_controller import TrInternaController
 
 
 def interna_constructor(entrada: Dict):
     data_pagameto = entrada['data']
     origem = entrada['origem']
     contas = DadosDeContas()
-    fornecedores = DadosDeFornecedores()
-    pagamentos = DadosDePagamentoRepository()
+    fornecedores = None
+    pagamentos = None
 
-    tranferencias = TransferController(contas, data_pagameto, fornecedores, origem, pagamentos)
+    tranferencias = TrInternaController(contas, data_pagameto, fornecedores, origem, pagamentos)
     dados_de_tranferencias = tranferencias.filtrar_dados()
 
     for dicionario in dados_de_tranferencias:
